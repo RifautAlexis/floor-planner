d1b4ad10fb60f6c40d89e802e1e75c64ba8613e5
Rifaut Alexis <ralexis149@gmail.com>
2024-05-12 22:34:41 +0200

    Add drawing grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnInit, inject } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, inject, viewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
@@ -18,117 +18,91 @@ import {
 export class AppComponent implements OnInit, AfterViewInit {
   window = inject(Window);
 
-  camera!: THREE.Camera;
-  scene!: THREE.Scene;
-  renderer!: THREE.WebGLRenderer;
-  orbitControl!: OrbitControls;
-  labelRenderer!: CSS2DRenderer;
+  /** The canvas element. */
+  private canvasElement!: HTMLCanvasElement;
 
-  width: number = window.innerWidth;
-  height: number = window.innerHeight;
+  /** The 2D context. */
+  private context!: CanvasRenderingContext2D;
 
-  ctrlDown = false;
-  lineId = 0;
-  line!: THREE.Line;
-  drawingLine = false;
-  readonly measurementLabels: { [key: number]: CSS2DObject } = {};
-
-  readonly raycaster = new THREE.Raycaster();
-  intersects!: THREE.Intersection[];
-  readonly mouse = new THREE.Vector2();
+  // grid parameters
+  gridSpacing = 20; // pixels
+  gridWidth = 1;
+  gridColor = "#f1f1f1";
 
   ngOnInit(): void {
+    this.canvasElement = <HTMLCanvasElement>document.getElementById('floorplanner-canvas');
+    this.context = this.canvasElement.getContext('2d')!;
+
     this.init();
     this.animate();
-
-    window.addEventListener('keydown', (event) => {
-      if (event.key === 'Control') {
-          console.log('TRUE');
-          this.ctrlDown = true
-          this.orbitControl.enabled = false
-          this.renderer.domElement.style.cursor = 'crosshair'
-      }
-    });
-    
-    window.addEventListener('keyup', (event) => {
-        if (event.key === 'Control') {
-            console.log("FALSE");
-            this.ctrlDown = false
-            this.orbitControl.enabled = true
-            this.renderer.domElement.style.cursor = 'pointer'
-            if (this.drawingLine) {
-                //delete the last line because it wasn't committed
-                this.scene.remove(this.line)
-                this.scene.remove(this.measurementLabels[this.lineId])
-                this.drawingLine = false
-            }
-        }
-    });
-
-    
-    // this.renderer.domElement.addEventListener('pointerdown', this.onClick, false);
-    this.renderer.domElement.addEventListener('pointerdown', (event: PointerEvent) => {
-      console.log(( event.clientX / this.renderer.domElement.clientWidth ) * 2 - 1,
-      - ( event.clientY / this.renderer.domElement.clientHeight ) * 2 + 1,);
-    }, false);
   }
 
   ngAfterViewInit(): void {
-    // const plane = new THREE.Plane( new THREE.Vector3( 1, 1, 0 ), 0 );
-    // const planeHelper = new THREE.PlaneHelper( plane, 1, 0xffff00 );
-    
-    var grid = new THREE.GridHelper( 200, 10 );
-    this.scene.add( grid );
-
-    this.renderer.render(this.scene, this.camera);
   }
 
   private init() {
-    const viewSize = 900;
-    const aspectRation = this.width / this.height;
-    this.camera = new THREE.OrthographicCamera(
-      -aspectRation*viewSize / 2,
-      aspectRation*viewSize / 2,
-      viewSize / 2,
-      -viewSize / 2,
-      -1000,
-      1000
-    );
-    this.camera.position.set(0, 90, 0);
-
-    this.renderer = new THREE.WebGLRenderer({ antialias: true });
-    this.renderer.setSize(this.width, this.height);
-    document.body.appendChild(this.renderer.domElement);
-
-    this.scene = new THREE.Scene();
-    this.scene.background = new THREE.Color(0xFFFFFF);
-
-    this.labelRenderer = new CSS2DRenderer();
-    this.labelRenderer.setSize(window.innerWidth, window.innerHeight);
-    this.labelRenderer.domElement.style.position = 'absolute';
-    this.labelRenderer.domElement.style.top = '0px';
-    this.labelRenderer.domElement.style.pointerEvents = 'none';
-    document.body.appendChild(this.labelRenderer.domElement);
-
-    this.orbitControl = new OrbitControls(this.camera, this.renderer.domElement);
-    this.orbitControl.enableRotate = false;
-    this.orbitControl.minZoom = 1;
-    this.orbitControl.maxZoom = 10;
-    // this.orbit.target.set(0, 310, 0);
-    this.orbitControl.update();
+    this.handleWindowResize();
   }
 
-  private animate() {
-           
-    requestAnimationFrame(() => this.animate());
-    this.renderer.render(this.scene, this.camera);
+  private animate() { }
 
+  /** */
+  public handleWindowResize() {
+    this.canvasElement.height = this.window.innerHeight;
+    this.canvasElement.width = parent.innerWidth;
+    this.draw();
   }
 
-  // private onClick(popo: HTMLCanvasElement, ev: PointerEvent) {
-  //   console.log(this.ctrlDown);
-  //   if (this.ctrlDown) {
-  //     console.log(event.clientX, event.clientY);
-  //   }
-  // }
+  /** */
+  public draw() {
+    this.context.clearRect(0, 0, this.canvasElement.width, this.canvasElement.height);
+
+    this.drawGrid();
+
+    // if (this.viewmodel.mode == floorplannerModes.DRAW) {
+    //   this.drawTarget(this.viewmodel.targetX, this.viewmodel.targetY, this.viewmodel.lastNode);
+    // }
+
+    // this.floorplan.getWalls().forEach((wall) => {
+    //   this.drawWallLabels(wall);
+    // });
+  }
+
+  /** */
+  private drawGrid() {
+    const originX = 0;
+    const originY = 0;
+    var offsetX = this.calculateGridOffset(-originX);
+    var offsetY = this.calculateGridOffset(-originY);
+    var width = this.canvasElement.width;
+    var height = this.canvasElement.height;
+    for (var x = 0; x <= (width / this.gridSpacing); x++) {
+      this.drawLine(this.gridSpacing * x + offsetX, 0, this.gridSpacing * x + offsetX, height, this.gridWidth, this.gridColor);
+    }
+    for (var y = 0; y <= (height / this.gridSpacing); y++) {
+      this.drawLine(0, this.gridSpacing * y + offsetY, width, this.gridSpacing * y + offsetY, this.gridWidth, this.gridColor);
+    }
+  }
+
+  /** returns n where -gridSize/2 < n <= gridSize/2  */
+  private calculateGridOffset(n: number) {
+    if (n >= 0) {
+      return (n + this.gridSpacing / 2.0) % this.gridSpacing - this.gridSpacing / 2.0;
+    } else {
+      return (n - this.gridSpacing / 2.0) % this.gridSpacing + this.gridSpacing / 2.0;
+    }
+  }
+
+  /** */
+  private drawLine(startX: number, startY: number, endX: number, endY: number, width: number, color: string) {
+    // width is an integer
+    // color is a hex string, i.e. #ff0000
+    this.context.beginPath();
+    this.context.moveTo(startX, startY);
+    this.context.lineTo(endX, endY);
+    this.context.lineWidth = width;
+    this.context.strokeStyle = color;
+    this.context.stroke();
+  }
+
 }
